6f8f46f641e4ec728f0a870439e7ac69c6b2b11f
DESKTOP-EUKB5FE\ceo99 <33651801+ceo991@users.noreply.github.com>
2021-10-25 00:21:24 +0300

    Main content is removed from About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="about">
-    <Message/>
+    <!--<HelloWorld/>-->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Message from "@/components/HelloWorld.vue";
+//import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "About",
   components: {
-    Message,
+    //HelloWorld,
   },
 };
 </script>
