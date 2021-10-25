a3b89e558283cb4bbc23696b34c67176067d877b
DESKTOP-EUKB5FE\ceo99 <33651801+ceo991@users.noreply.github.com>
2021-10-25 22:18:00 +0300

    something changed

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,29 +1,27 @@
 <template>
   <div class="about">
-<<<<<<< HEAD
+
     <!--<HelloWorld/>-->
-=======
+
     <!--<Message/>-->
->>>>>>> 73bfb78837f99e9c439e50f7d81820b0bc4cb2c9
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-<<<<<<< HEAD
+
 //import HelloWorld from "@/components/HelloWorld.vue";
-=======
+
 //import Message from "@/components/HelloWorld.vue";
->>>>>>> 73bfb78837f99e9c439e50f7d81820b0bc4cb2c9
+
 
 export default {
   name: "About",
   components: {
-<<<<<<< HEAD
+
     //HelloWorld,
-=======
     //Message,
->>>>>>> 73bfb78837f99e9c439e50f7d81820b0bc4cb2c9
-  },
-};
+  }
+}
 </script>
