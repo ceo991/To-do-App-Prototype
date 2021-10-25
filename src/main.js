b0ed08a36ba1c0e07abf7a948dc93fe679e90973
ceo991 <ceo991@hotmail.com>
2021-10-25 15:06:09 +0300

    cards added

--- src/main.js
+++ src/main.js
@@ -5,9 +5,7 @@ import store from "./store";
 import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import VueTailwind from 'vue-tailwind';
 
-Vue.use(VueTailwind);
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
